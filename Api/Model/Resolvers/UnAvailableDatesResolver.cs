682c61a88ede83d4909e4f32641eeee1c727294f
Dominique Karreman <90785931+DominiqueKarreman@users.noreply.github.com>
2023-04-13 23:42:42 +0200

    Update UnAvailableDatesResolver.cs

--- Api/Model/Resolvers/UnAvailableDatesResolver.cs
+++ Api/Model/Resolvers/UnAvailableDatesResolver.cs
@@ -1,6 +1,33 @@
-﻿namespace Api.Model.Resolvers
+using Api.Model.DTO;
+using AutoMapper;
+
+namespace Api.Model.Resolvers
 {
-   public class Class
+   public class UnAvailableDatesResolver : IValueResolver<List<Reservation>, UnAvailableDatesDto, List<DateTime>>
    {
+      public List<DateTime> Resolve(List<Reservation> source, UnAvailableDatesDto destination, List<DateTime> destMember, ResolutionContext context)
+      {
+         var unAvailableDates = new List<DateTime>();
+
+         foreach (var reservation in source)
+         {
+            var residence = new ResidenceFromDto(reservation.StartDate, reservation.EndDate);
+            var dates = new List<DateTime>();
+            for (
+                var date = residence.StartDate.AddDays(1);
+                date <= residence.EndDate.AddDays(1);
+                date = date.AddDays(1)
+            )
+            {
+               dates.Add(date);
+            }
+            residence.Dates = dates;
+            unAvailableDates.AddRange(residence.Dates.Select(date => date.Date.AddHours(22)));
+         }
+
+         return unAvailableDates;
+      }
    }
+
+
 }
