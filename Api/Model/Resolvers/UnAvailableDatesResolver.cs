24053bf581b1ec338ca6d83a36b892b7e23740ff
Dominique Karreman <s1169919@student.windesheim.nl>
2023-06-02 12:10:23 +0200

    made homescreen start, programme div done

--- Api/Model/Resolvers/UnAvailableDatesResolver.cs
+++ Api/Model/Resolvers/UnAvailableDatesResolver.cs
@@ -3,31 +3,32 @@ using AutoMapper;
 
 namespace Api.Model.Resolvers
 {
-   public class UnAvailableDatesResolver : IValueResolver<List<Reservation>, UnAvailableDatesDto, List<DateTime>>
-   {
-      public List<DateTime> Resolve(List<Reservation> source, UnAvailableDatesDto destination, List<DateTime> destMember, ResolutionContext context)
-      {
-         var unAvailableDates = new List<DateTime>();
+    public class UnAvailableDatesResolver : IValueResolver<List<Reservation>, UnAvailableDatesDto, List<DateTime>>
+    {
+        public List<DateTime> Resolve(List<Reservation> source, UnAvailableDatesDto destination, List<DateTime> destMember, ResolutionContext context)
+        {
+            var unAvailableDates = new List<DateTime>();
 
-         foreach (var reservation in source)
-         {
-            var residence = new ResidenceFromDto(reservation.StartDate, reservation.EndDate);
-            var dates = new List<DateTime>();
-            for (
-                var date = residence.StartDate.AddDays(1);
-                date <= residence.EndDate.AddDays(1);
-                date = date.AddDays(1)
-            )
+            foreach (var reservation in source)
             {
-               dates.Add(date);
-            }
-            residence.Dates = dates;
-            unAvailableDates.AddRange(residence.Dates.Select(date => date.Date.AddHours(22)));
-         }
+                var residence = new ResidenceFromDto(reservation.StartDate, reservation.EndDate);
+                var dates = new List<DateTime>();
+                for (
+                    var date = residence.StartDate.AddDays(1);
+                    date <= residence.EndDate.AddDays(1);
+                    date = date.AddDays(1)
+                )
+                {
+                    dates.Add(date);
+                }
+                residence.Dates = dates;
 
-         return unAvailableDates;
-      }
-   }
+            unAvailableDates.AddRange(residence.Dates.Select(date => date.Date.AddHours(22)));
+            }
+
+            return unAvailableDates;
+        }
+    }
 
 
 }
